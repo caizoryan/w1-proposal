6fd245adf15337315fa59c5ddefb0fb419c4bc6a
Aaryan Pashine <pashineaaryan@gmail.com>
2025-08-22 12:35:54 +0530

    no added server and data...

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express"
+import cors from "cors"
+import fs from "fs"
+
+const app = express()
+app.use(cors())
+app.use(express.json());
+app.use(express.static('.'))
+
+let port = 7462
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`)
+})
+
+app.post('/save', (req, res) => {
+	console.log(req.body)
+	fs.writeFileSync('./data.json', JSON.stringify(req.body, null, 2))
+	let read = fs.readFileSync('./data.json', {encoding: 'utf-8'})
+	res.send({wrote: read})
+})
